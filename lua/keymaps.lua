f42831ba4745e318eb94ee8f8271082333d30251
Seezar75 <cesare.ali@gmail.com>
2023-12-19 14:52:57 +0100

    terminal keybindings and adjustments

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,5 @@
 
--- telescope key bindings
+-- wxelescope key bindings
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
@@ -10,20 +10,35 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz', {})
 vim.keymap.set('n', '<C-u>', '<C-u>zz', {})
 
 -- move lines and hold on to yanked text
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", {})
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", {})
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", {})
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", {})
 vim.keymap.set("v", "p", '"_dP', opts)
 
--- custom keymaps
+-- exit insert mode with jk  
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true })
+
 --vim.keymap.set("n", "<leader>e", ":Lex 30<cr>", { noremap = true, silent = true })
+
 -- window navigation
 vim.keymap.set("n", "<leader>wl", "<C-w>l", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>wh", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>wj", "<C-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>wk", "<C-w>k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>wv", "<C-w>v", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>wh", "<C-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>wh", "<C-w>s", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>wx", ":close<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("n", "<C-n>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+
+-- terminal navigation
+vim.keymap.set("n", "<leader>t", ":split term://bash<CR>i", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-Up>", "<cmd>resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("t", "<C-Down>", "<cmd>resize -2<CR>", { noremap = true, silent = true })
